859da9038c009b47f2f9370ed2690532ff70bdc5
saigetianxia <saigetianxia@gmail.com>
2025-02-08 12:43:12 +0800

    Update sb_00.sh

--- sb_00.sh
+++ sb_00.sh
@@ -577,7 +577,7 @@ quick_command() {
   SCRIPT_PATH="$HOME/bin/$COMMAND"
   mkdir -p "$HOME/bin"
   echo "#!/bin/bash" > "$SCRIPT_PATH"
-  echo "bash <(curl -Ls https://raw.githubusercontent.com/eooce/sing-box/test/sb_00.sh)" >> "$SCRIPT_PATH"
+  echo "bash <(curl -Ls https://raw.githubusercontent.com/saigetianxia/Sing-box/test/sb_00.sh)" >> "$SCRIPT_PATH"
   chmod +x "$SCRIPT_PATH"
   if [[ ":$PATH:" != *":$HOME/bin:"* ]]; then
       echo "export PATH=\"\$HOME/bin:\$PATH\"" >> "$HOME/.bashrc"
